1f188d4c4a32d9b0b77920955fb9715ca2db0d2b
Tanish018 <tanishrathore222@gmail.com>
2025-12-29 23:03:23 +0530

    final changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,10 +7,10 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import { app, server, io } from './lib/socket.js'
 import path from 'path'
+import { fileURLToPath } from "url";
 
 dotenv.config()
 const PORT = process.env.PORT
-const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -22,15 +22,18 @@ app.use(cors(
     }
 ))
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendPath = path.resolve(__dirname, "../../frontend/dist");
+
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../Frontend/dist")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"))
-    })
+  app.use(express.static(frontendPath));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(frontendPath, "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
