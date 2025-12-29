8c8ad890f9cd857be79455c898062d563b40f3de
Tanish018 <tanishrathore222@gmail.com>
2025-12-29 23:08:05 +0530

    ..

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/messages", messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
-  app.get(/.*/, (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(frontendPath, "index.html"));
   });
 }
